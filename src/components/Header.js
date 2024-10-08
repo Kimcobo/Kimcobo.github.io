5bfb39e33233d5244cf5ea5d0c74ae91b0489501
Kimcobo <kchanwoo1205@gmail.com>
2024-10-08 14:28:34 +0900

    profile v1

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,20 @@ import React from 'react';
 
 const Header=()=>{
     return(
-        <div>Header</div>
+        <header id='header'>
+            <div className='header_innerbox'>
+                <h1 className='logo'>Site Logo</h1>
+                <nav className='header_nav'>
+                    <ul>
+                        <li><a href='#intro'>Intro</a></li>
+                        <li><a href='#ability'>Ability</a></li>
+                        <li><a href='#site'>Site</a></li>
+                        <li><a href='#project'>Project</a></li>
+                        <li><a href='#contact'>Contact</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
     )
 }
 
